b558cf80fc655d92d47407feeada429908d34f2b
1Cbondy1 <claytonbondy@gmail.com>
2019-05-29 00:05:06 -0500

    created Header, Footer, MainContent components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div>
-      <p>This is the footer.</p>
-    </div>
+    <footer id="footer">
+        <span className="links">
+            <a href="mailto:cbondy@slicevisualstudio.com"><i className="fa fa-envelope"></i></a>
+        </span>
+        <span className="links">
+            <a href="tel:225-718-5327"><i className="fa fa-phone"></i></a>
+        </span>
+        <span className="links">
+            <a href="https://www.linkedin.com/in/clayton-bondy-52953893/"><i className="fa fa-linkedin-square"></i></a>
+        </span>
+        <span className="links">
+            <a href="https://www.instagram.com/"><i className="fa fa-instagram"></i></a>
+        </span>
+    </footer>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
 
 function Header() {
   return (
     <div>
-      <p>This is the header.</p>
+
+      <Nav className="justify-content-end">
+
+      <Nav.Item>
+        <Nav.Link eventKey="link-0"><img id="nav-logo" src="images/SVS_text-logo.jpg" alt="slice-logo" /></Nav.Link>
+      </Nav.Item>
+
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">About</Nav.Link>
+      </Nav.Item>
+
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Services</Nav.Link>
+      </Nav.Item>
+
+      <Nav.Item>
+        <Nav.Link eventKey="link-3">Portfolio</Nav.Link>
+      </Nav.Item>
+
+      <Nav.Item>
+        <Nav.Link eventKey="link-4">Pricing</Nav.Link>
+      </Nav.Item>
+
+      <Nav.Item>
+        <Nav.Link eventKey="link-4">Contact</Nav.Link>
+      </Nav.Item>
+
+      </Nav>
+
     </div>
   );
 }
