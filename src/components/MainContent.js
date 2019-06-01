628c8e8d5d76df99a677fad159fac6fc640f95e2
1Cbondy1 <claytonbondy@gmail.com>
2019-06-01 17:04:15 -0500

    we have the best words

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -8,7 +8,7 @@ class MainContent extends React.Component {
   render() {
     return (
       <div className="extra">
-      
+
         <img id="main-image" src="./images/main-image.jpg" alt="main" />
   
         <Container >
@@ -17,7 +17,7 @@ class MainContent extends React.Component {
   
           <div className="group-margin section1">
             <p><strong>WHAT IS SLICE VISUAL STUDIO?</strong></p>
-            <p>Slice is a dynamic team of professionals experienced in architectural visualization, 3D modeling, and rendering.</p>
+            <p className="text-box">Slice is a visualization studio based in Austin, Texas. We translate architectural designs into captivating and engaging imagery. Our dynamic team is experienced in 3D modeling, rendering, and drafting.</p>
           </div>
   
           <hr />
@@ -26,7 +26,7 @@ class MainContent extends React.Component {
             <p><strong>2D AND 3D VISUALIZATION SERVICES</strong></p>
             <img className="viz-images" src="./images/services-01.jpg" alt="viz" />
             <img className="viz-images" src="./images/services-00.jpg" alt="viz" />
-            <p>We create high quality 2D and 3D marketing floor plans for real estate agents, property managers, and developers.</p>
+            <p className="text-box"> We specialize in creating high quality 2D and 3D marketing floor plans for real estate agents, property managers, and developers.</p>
           </div>
   
           <hr />
