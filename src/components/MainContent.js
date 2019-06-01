a152657cedd61161b43ab8090d8f670aef29b8a1
1Cbondy1 <claytonbondy@gmail.com>
2019-05-31 19:43:28 -0500

    styled PortfolioCard component and added modal

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,52 +3,54 @@ import Container from 'react-bootstrap/Container';
 
 import PortfolioCard from './PortfolioCard';
 
-function MainContent() {
-  return (
-    <div>
-      <img id="main-image" src="./images/main-image.jpg" alt="main" />
-
-      <Container>
-
-        <hr />
-
-        <div className="group-margin">
-          <p><strong>WHAT IS SLICE VISUAL STUDIO?</strong></p>
-          <p>Slice is a dynamic team of professionals experienced in architectural visualization, 3D modeling, and rendering.</p>
-          <p>We create high quality 2D and 3D marketing floor plans for real estate agents, property managers, and developers.</p>
-        </div>
-
-        <hr />
-
-        <div className="group-margin">
-          <p><strong>2D AND 3D VISUALIZATION SERVICES</strong></p>
-          <img className="viz-images" src="./images/evolve-A-plan-sq.jpg" alt="viz" />
-          <img className="viz-images" src="./images/evolve-A-00-sq.jpg" alt="viz" />
-        </div>
-
-        <hr />
-
-        <div className="group-margin">
-          <p><strong>PORTFOLIO</strong></p>
-          <PortfolioCard image="./images/evolve-A-plan-sq.jpg" />
-          <PortfolioCard image="./images/evolve-A-00-sq.jpg" />
-          <PortfolioCard image="./images/evolve-A-01-sq.jpg" />
-          <PortfolioCard image="./images/evolve-B-plan-sq.jpg" />
-          <PortfolioCard image="./images/evolve-B-00-sq.jpg" />
-          <PortfolioCard image="./images/evolve-B-01-sq.jpg" />
-        </div>
-
-        <hr />
-
-        <div className="group-margin">
-          <p><strong>PRICING CHART</strong></p>
-          <img id="chart" src="./images/price-chart.jpg" alt="chart" />
-        </div>
-
-      </Container>
-
-    </div>
-  );
+class MainContent extends React.Component {
+  render() {
+    return (
+      <div>
+        <img id="main-image" src="./images/main-image.jpg" alt="main" />
+  
+        <Container>
+  
+          <hr />
+  
+          <div className="group-margin">
+            <p><strong>WHAT IS SLICE VISUAL STUDIO?</strong></p>
+            <p>Slice is a dynamic team of professionals experienced in architectural visualization, 3D modeling, and rendering.</p>
+          </div>
+  
+          <hr />
+  
+          <div className="group-margin">
+            <p><strong>2D AND 3D VISUALIZATION SERVICES</strong></p>
+            <img className="viz-images" src="./images/evolve-A-plan-sq.jpg" alt="viz" />
+            <img className="viz-images" src="./images/evolve-A-00-sq.jpg" alt="viz" />
+            <p>We create high quality 2D and 3D marketing floor plans for real estate agents, property managers, and developers.</p>
+          </div>
+  
+          <hr />
+  
+          <div className="group-margin">
+            <p><strong>PORTFOLIO</strong></p>
+            <PortfolioCard image="./images/evolve-A-plan-sq.jpg" />
+            <PortfolioCard image="./images/evolve-A-00-sq.jpg" />
+            <PortfolioCard image="./images/evolve-A-01-sq.jpg" />
+            <PortfolioCard image="./images/evolve-B-plan-sq.jpg" />
+            <PortfolioCard image="./images/evolve-B-00-sq.jpg" />
+            <PortfolioCard image="./images/evolve-B-01-sq.jpg" />
+          </div>
+  
+          <hr />
+  
+          <div className="group-margin">
+            <p><strong>PRICING CHART</strong></p>
+            <img id="chart" src="./images/price-chart.jpg" alt="chart" />
+          </div>
+  
+        </Container>
+  
+      </div>
+    );
+  }
 }
 
 export default MainContent;
--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import Modal from 'react-bootstrap/Modal';
 
-function PortfolioCard(props) {
-  return (
-    <span>
-      <img className="portfolio-images" src={props.image} alt="portfolio" />
-    </span>
-  );
+
+class MyVerticallyCenteredModal extends React.Component {
+  render(props) {
+    return (
+      <Modal
+        {...this.props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Body>
+          <span>
+            <img className="modal-image" src={this.props.image} alt="portfolio" />
+          </span>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+}
+
+class PortfolioCard extends React.Component {
+  constructor(...args) {
+    super(...args);
+
+    this.state = { modalShow: false };
+  }
+
+  render() {
+    let modalClose = () => this.setState({ modalShow: false });
+
+    return (
+        <span>
+          <span>
+            <img onClick={() => this.setState({ modalShow: true })} className="portfolio-images" src={this.props.image} alt="portfolio" />
+          </span>
+
+          <MyVerticallyCenteredModal
+            show={this.state.modalShow}
+            onHide={modalClose}
+            image={this.props.image}
+          />
+        </span>
+    );
+  }
 }
 
 export default PortfolioCard;
