b7e5271eb454c5795f14e9c24622fec86fc025a3
1Cbondy1 <claytonbondy@gmail.com>
2019-06-01 16:28:04 -0500

    cleaned up code, added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,18 +8,8 @@ function Header() {
     return (
         <div>
 
-            {/* <img id="nav-logo" src="./images/text-logo-alpha.png" alt="logo-alpha"/> */}
-
-            {/* <Nav className="justify-content-end">
-                <NavItem eventKey="link-1" to="section1" title="About" />
-                <NavItem eventKey="link-2" to="section2" title="Services" />
-                <NavItem eventKey="link-3" to="section3" title="Portfolio" />
-                <NavItem eventKey="link-4" to="section4" title="Pricing" />
-                <NavItem eventKey="link-5" to="section5" title="Contact" />
-            </Nav> */}
-
             <Navbar className="nav" expand="lg" variant="dark">
-                <Navbar.Brand href="#home">Slice Visual Studio</Navbar.Brand>
+                <Navbar.Brand><img id="nav-logo" src="./images/text-logo-alpha.png" alt="logo-alpha"/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
