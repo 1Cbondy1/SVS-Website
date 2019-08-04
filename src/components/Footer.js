0ef35a7c24582d087a3969351f1beed99a8058b0
1Cbondy1 <claytonbondy@gmail.com>
2019-08-04 15:12:15 -0500

    footer links + homepage fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,16 @@ function Footer() {
   return (
     <footer className="section5" id="footer">
         <span>
-            <a className="links" href="mailto:cbondy@slicevisualstudio.com"><i className="fa fa-envelope"></i></a>
+            <a className="links" href="mailto:clayton.bondy@slicevisualstudio.com"><i className="fa fa-envelope"></i></a>
         </span>
         <span>
             <a className="links" href="tel:225-718-5327"><i className="fa fa-phone"></i></a>
         </span>
-        <span>
+        {/* <span>
             <a className="links" href="https://www.linkedin.com/in/clayton-bondy-52953893/"><i className="fa fa-linkedin-square"></i></a>
-        </span>
+        </span> */}
         <span>
-            <a className="links" href="https://www.instagram.com/"><i className="fa fa-instagram"></i></a>
+            <a className="links" href="https://www.instagram.com/slicevisualstudio/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a>
         </span>
     </footer>
   );
