7fb6617450cb7fcffd46e2946b41555b1e195150
1Cbondy1 <claytonbondy@gmail.com>
2020-03-29 23:06:14 -0500

    add accordion and reorder

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,8 +14,8 @@ function Header() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <NavItem eventKey="link-1" to="section1" title="About" />
-                        <NavItem eventKey="link-2" to="section2" title="Services" />
                         <NavItem eventKey="link-3" to="section3" title="Portfolio" />
+                        <NavItem eventKey="link-2" to="section2" title="Services" />
                         <NavItem eventKey="link-4" to="section4" title="Pricing" />
                         <NavItem eventKey="link-5" to="section5" title="Contact" />
                     </Nav>
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
 
 import PortfolioCard from './PortfolioCard';
 // import ChartCard from './ChartCard';
@@ -13,26 +15,17 @@ class MainContent extends React.Component {
         <img id="main-image" src="./images/main-image.png" alt="main" />
   
         <Container >
-  
-          <hr />
-  
-          <div className="group-margin section1">
+
+        <hr />
+
+        <div className="group-margin section1">
             <p><strong>WHAT IS SLICE VISUAL STUDIO?</strong></p>
-            <p className="text-box">Slice is a visualization studio based in Austin, Texas. We translate architectural designs into captivating and engaging imagery. Our dynamic team is experienced in 3D modeling, rendering, and drafting.</p>
-          </div>
-  
-          <hr />
-  
-          <div className="group-margin section2">
-            <p><strong>2D AND 3D VISUALIZATION SERVICES</strong></p>
-            <img className="viz-images" src="./images/services-01.png" alt="viz" />
-            <img className="viz-images" src="./images/services-00.png" alt="viz" />
-            <p className="text-box"> We specialize in creating high quality 2D and 3D marketing floor plans for real estate agents, property managers, and developers.</p>
-          </div>
-  
-          <hr />
-  
-          <div className="group-margin section3">
+            <p className="text-box">Slice is a visualization studio based in Austin, Texas. We translate scenic and architectural designs into captivating and engaging imagery. Our dynamic team is skilled in 3D rendering, animation, and 360° VR rendering and photography.</p>
+        </div>
+
+        <hr />
+
+        <div className="group-margin section3">
             <p><strong>PORTFOLIO</strong></p>
             <PortfolioCard image="./images/web_seven-apts-bremond-01.jpg" />
             <PortfolioCard image="./images/web_seven-apts-bremond-00.jpg" />
@@ -46,16 +39,32 @@ class MainContent extends React.Component {
             <PortfolioCard image="./images/web_evolve-B-01.jpg" />
             <PortfolioCard image="./images/web_evolve-B-00.jpg" />
             <PortfolioCard image="./images/web_evolve-B-plan.jpg" />
-          </div>
-  
-          <hr />
-  
-          <div className="group-margin section4">
-            <p><strong>FLOOR PLAN PRICING</strong></p>
-            <img id="chart" src="./images/price-chart.jpg" alt="pricing-chart" />
-          </div>
-  
-        </Container>
+        </div>
+
+        <hr />
+
+        <div className="group-margin section2">
+            <p><strong>2D AND 3D FLOOR PLAN SERVICES</strong></p>
+            <img className="viz-images" src="./images/services-01.png" alt="viz" />
+            <img className="viz-images" src="./images/services-00.png" alt="viz" />
+            <p className="text-box"> We specialize in creating high quality 2D and 3D marketing floor plans for real estate agents, property managers, and developers.</p>
+        </div>
+
+        <hr />
+
+        <div className="group-margin section4">
+            <Accordion>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                    <p><strong>FLOOR PLAN PRICING ▼</strong></p>
+                </Accordion.Toggle>
+
+                <Accordion.Collapse eventKey="0">
+                    <img id="chart" src="./images/price-chart.jpg" alt="pricing-chart" />
+                </Accordion.Collapse>
+            </Accordion>
+        </div>
+
+    </Container>
   
       </div>
     );
