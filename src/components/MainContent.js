b3447d8f77af28349a5f74dc94b1696d0d31919e
1Cbondy1 <claytonbondy@gmail.com>
2020-04-21 21:24:30 -0500

    add rotated state for accordion trigger

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -8,69 +8,78 @@ import PortfolioCard from './PortfolioCard';
 import { CLIENTS } from '../constants/enums'
 
 class MainContent extends React.Component {
-  render() {
-    return (
-      <div className="extra">
+    state = {
+        rotated: false
+    };
 
-        <img id="main-image" src="./images/main-image.png" alt="main" />
-  
-        <Container >
+    handleClick = () =>
+        this.setState(
+            prevState => ({ rotated: !prevState.rotated })
+        );
 
-        <hr />
+    render() {
+        return (
+            <div className="extra">
 
-        <div className="group-margin section1">
-            <p><strong>WHAT IS SLICE VISUAL STUDIO?</strong></p>
-            <p className="text-box">Slice is a visualization studio based in Austin, Texas. We translate scenic and architectural designs into captivating and engaging imagery. Our dynamic team is skilled in 3D rendering, animation, and 360° VR rendering and photography.</p>
-        </div>
+                <img id="main-image" src="./images/main-image.png" alt="main" />
 
-        <hr />
+                <Container >
 
-        <div className="group-margin section3">
-            <p><strong>PORTFOLIO</strong></p>
-            <PortfolioCard image="./images/web_seven-apts-bremond-01.jpg" />
-            <PortfolioCard image="./images/web_seven-apts-bremond-00.jpg" />
-            <PortfolioCard image="./images/BMB-01.jpg" credit={CLIENTS.CSD}/>
-            <PortfolioCard image="./images/BMB-02.jpg" credit={CLIENTS.CSD}/>
-            <PortfolioCard image="./images/web_evolve-A-01.jpg" />
-            <PortfolioCard image="./images/web_evolve-A-00.jpg" />
-            <PortfolioCard image="./images/web_evolve-A-plan.jpg" />
-            <PortfolioCard image="./images/Walmart-02.jpg" credit={CLIENTS.YD}/>
-            <PortfolioCard image="./images/Walmart-01.jpg" credit={CLIENTS.YD}/>
-            <PortfolioCard image="./images/web_evolve-B-01.jpg" />
-            <PortfolioCard image="./images/web_evolve-B-00.jpg" />
-            <PortfolioCard image="./images/web_evolve-B-plan.jpg" />
-            <PortfolioCard image="./images/NCL-Laser-Tag-03.jpg" credit={CLIENTS.YD}/>
-            <PortfolioCard image="./images/NCL-Laser-Tag-02.jpg" credit={CLIENTS.YD}/>
-        </div>
+                <hr />
 
-        <hr />
+                <div className="group-margin section1">
+                    <p><strong>WHAT IS SLICE VISUAL STUDIO?</strong></p>
+                    <p className="text-box">Slice is a visualization studio based in Austin, Texas. We translate scenic and architectural designs into captivating and engaging imagery. Our dynamic team is skilled in 3D rendering, animation, and 360° VR rendering and photography.</p>
+                </div>
 
-        <div className="group-margin section2">
-            <p><strong>2D AND 3D FLOOR PLAN SERVICES</strong></p>
-            <img className="viz-images" src="./images/services-01.png" alt="viz" />
-            <img className="viz-images" src="./images/services-00.png" alt="viz" />
-            <p className="text-box"> We specialize in creating high quality 2D and 3D marketing floor plans for real estate agents, property managers, and developers.</p>
-        </div>
+                <hr />
 
-        <hr />
+                <div className="group-margin section2">
+                    <p><strong>PORTFOLIO</strong></p>
+                    <PortfolioCard image="./images/web_seven-apts-bremond-01.jpg" />
+                    <PortfolioCard image="./images/web_seven-apts-bremond-00.jpg" />
+                    <PortfolioCard image="./images/BMB-01.jpg" credit={CLIENTS.CSD}/>
+                    <PortfolioCard image="./images/BMB-02.jpg" credit={CLIENTS.CSD}/>
+                    <PortfolioCard image="./images/web_evolve-A-01.jpg" />
+                    <PortfolioCard image="./images/web_evolve-A-00.jpg" />
+                    <PortfolioCard image="./images/web_evolve-A-plan.jpg" />
+                    <PortfolioCard image="./images/Walmart-02.jpg" credit={CLIENTS.YD}/>
+                    <PortfolioCard image="./images/Walmart-01.jpg" credit={CLIENTS.YD}/>
+                    <PortfolioCard image="./images/web_evolve-B-01.jpg" />
+                    <PortfolioCard image="./images/web_evolve-B-00.jpg" />
+                    <PortfolioCard image="./images/web_evolve-B-plan.jpg" />
+                    <PortfolioCard image="./images/NCL-Laser-Tag-03.jpg" credit={CLIENTS.YD}/>
+                    <PortfolioCard image="./images/NCL-Laser-Tag-02.jpg" credit={CLIENTS.YD}/>
+                </div>
 
-        <div className="group-margin section4">
-            <Accordion>
-                <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                    <p><strong>FLOOR PLAN PRICING ▼</strong></p>
-                </Accordion.Toggle>
+                <hr />
 
-                <Accordion.Collapse eventKey="0">
-                    <img id="chart" src="./images/price-chart.jpg" alt="pricing-chart" />
-                </Accordion.Collapse>
-            </Accordion>
-        </div>
+                <div className="group-margin section3">
+                    <p><strong>2D AND 3D FLOOR PLAN SERVICES</strong></p>
+                    <img className="viz-images" src="./images/services-01.png" alt="viz" />
+                    <img className="viz-images" src="./images/services-00.png" alt="viz" />
+                    <p className="text-box"> We specialize in creating high quality 2D and 3D marketing floor plans for real estate agents, property managers, and developers.</p>
+                </div>
 
-    </Container>
-  
-      </div>
-    );
-  }
+                <hr />
+
+                <div className="group-margin section4">
+                    <Accordion>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="0" onClick={this.handleClick}>
+                            <p><strong>FLOOR PLAN PRICING<span className={this.state.rotated ? "trigger-rotated" : "trigger"}> ▼</span></strong></p>
+                        </Accordion.Toggle>
+
+                        <Accordion.Collapse eventKey="0">
+                            <img id="chart" src="./images/price-chart.jpg" alt="pricing-chart" />
+                        </Accordion.Collapse>
+                    </Accordion>
+                </div>
+
+            </Container>
+
+            </div>
+        );
+    }
 }
 
 export default MainContent;
